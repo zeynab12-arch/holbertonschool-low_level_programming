2bcfaccb8cbd592a5d4d288458e0da52361d2164
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 23:16:04 +0400

    Update 5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14
+ * followed by a new line.
+ *
+ * Return: void
+ */
+
+
+void more_numbers(void)
+{
+int i, n;
+
+for (i = 0; i < 10; i++)
+{
+for (n = 0; n <= 14; n++)
+{
+if (n > 9)
+_putchar('1');
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
+}
