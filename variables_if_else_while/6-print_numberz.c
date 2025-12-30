43d41c0d33659536e5937cd6803ce86a5abc3309
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 21:33:54 +0400

    Update 6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers from 0 to 9 using putchar.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 48; /* ASCII '0' */
+
+	while (n <= 57) /* ASCII '9' */
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
