42bb259c003beedeb8d73ca823d1901eb37ddd12
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-31 00:27:41 +0400

    Update 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,1 +1,30 @@
+#include "main.h"
 
+/**
+ * leet - encodes a string into 1337
+ * @str: string to encode
+ *
+ * Return: pointer to str
+ */
+char *leet(char *str)
+{
+int i = 0;
+int j;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "4433007711";
+
+while (str[i] != '\0')
+{
+j = 0;
+while (letters[j] != '\0')
+{
+if (str[i] == letters[j])
+{
+str[i] = numbers[j];
+}
+j++;
+}
+i++;
+}
+return (str);
+}
