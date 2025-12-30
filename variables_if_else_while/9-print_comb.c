dd9a4eda711da32a2f79cb729488301db8436a95
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 21:34:52 +0400

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers (0–9) separated by
+ * ", " using only putchar (max 4 calls).
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 48; /* ASCII '0' */
+
+	while (n <= 57)
+	{
+		putchar(n);
+
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
