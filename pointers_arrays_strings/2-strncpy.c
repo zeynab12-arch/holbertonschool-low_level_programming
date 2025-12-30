1e00249cc4c31490c754c8ce1388398cc316c267
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-31 00:21:59 +0400

    Update 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,29 @@
+#include "main.h"
 
+/**
+ * _strncpy - copies a string
+ * @dest: destination buffer
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+/* copy characters from src */
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+/* pad with null bytes if needed */
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+
+return (dest);
+}
