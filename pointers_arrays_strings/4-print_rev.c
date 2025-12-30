5e8a2a6dae27cdd8f67ca4a52532fe234b6c430a
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 23:47:15 +0400

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to the string
+ */
+void print_rev(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+len++;
+
+while (len > 0)
+_putchar(s[--len]);
+
+_putchar('\n');
+}
