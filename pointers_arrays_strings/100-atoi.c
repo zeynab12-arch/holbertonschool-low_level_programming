3b661623d6f4ef6faa93c89b77cc7b84f8a3fb3c
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 23:50:44 +0400

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,27 @@
+#include "main.h"
 
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value
+ */
+int _atoi(char *s)
+{
+int i = 0, sign = 1, num = 0;
+
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+{
+if (s[i] == '-')
+sign *= -1;
+i++;
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+if (num > (2147483647 - (s[i] - '0')) / 10)
+return (sign == 1 ? 2147483647 : -2147483648);
+num = num * 10 + (s[i] - '0');
+i++;
+}
+return (num *sign);
+}
