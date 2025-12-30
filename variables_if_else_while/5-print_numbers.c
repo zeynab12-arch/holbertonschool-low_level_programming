563d891666243ab11fe9e04e5f639d1e7fc74b07
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 21:33:35 +0400

    Update 5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+/**
+ * main - Prints all single-digit base 10 numbers starting from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
