e02a8b1adf68060481f9ba9f3ace878d47082c64
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-31 00:26:22 +0400

    Update 5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string to convert
+ *
+ * Return: pointer to str
+ */
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - ('a' - 'A');
+}
+i++;
+}
+
+return (str);
+}
