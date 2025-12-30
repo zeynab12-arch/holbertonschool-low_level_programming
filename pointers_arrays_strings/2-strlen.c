55ca88712e24536ec4165c38c76fa7854c4fd5ff
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 23:45:45 +0400

    Update 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+len++;
+
+return (len);
+}
