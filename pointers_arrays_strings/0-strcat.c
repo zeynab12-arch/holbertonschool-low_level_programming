055be92545d4083c765bc764b825d1699b92d1e6
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-31 00:20:38 +0400

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,32 @@
+#include "main.h"
 
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+
+/* find end of dest */
+while (dest[i] != '\0')
+{
+i++;
+}
+/* append src to dest */
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+
+/* add terminating null byte */
+dest[i] = '\0';
+
+return (dest);
+}
