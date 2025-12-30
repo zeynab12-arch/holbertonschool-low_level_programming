8cd17b9b1587abdfd8a2683db07a52cea11c9f05
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 23:47:45 +0400

    Update 5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string
+ */
+void rev_string(char *s)
+{
+int len = 0, i;
+char temp;
+
+while (s[len] != '\0')
+len++;
+
+for (i = 0; i < len / 2; i++)
+{
+temp = s[i];
+s[i] = s[len - 1 - i];
+s[len - 1 - i] = temp;
+}
+}
