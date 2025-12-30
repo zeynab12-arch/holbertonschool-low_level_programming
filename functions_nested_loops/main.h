84b8c7539ac8a02250d64d5ea8091cf2d72669fa
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 22:30:13 +0400

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
+void print_alphabet(void);
+int print_sign(int n);
+void print_alphabet_x10(void);
+int _isalpha(int c);
+int _islower(int c);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
