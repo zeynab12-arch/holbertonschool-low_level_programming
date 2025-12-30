e02126ee29bbdb61de99953084edd7ca59aab196
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 21:34:11 +0400

    Update 7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse (z to a)
+ * using only putchar (twice).
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c = 122; /* ASCII 'z' */
+
+	while (c >= 97) /* ASCII 'a' */
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
