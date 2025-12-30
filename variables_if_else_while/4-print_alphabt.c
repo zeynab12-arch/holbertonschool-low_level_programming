8d1e3625e4a6e7d4fad3c1cdd2f6d7b9ade72ed5
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 21:33:15 +0400

    Update 4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+/**
+ * main - prints the alphabet in lowercase except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	putchar('\n');
+
+	return (0);
+}
