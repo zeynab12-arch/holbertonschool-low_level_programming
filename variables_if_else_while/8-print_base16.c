dee053f59512c4ca1b38892316787821b1c336ef
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 21:34:31 +0400

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all hexadecimal digits in lowercase
+ * (0-9 then a-f) using only putchar (three calls allowed).
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 48; /* ASCII '0' */
+	int a = 97; /* ASCII 'a' */
+
+	while (n <= 57) /* 0-9 */
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (a <= 102) /* a-f */
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
