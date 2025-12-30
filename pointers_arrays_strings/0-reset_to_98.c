ba7e7b8c45e1bd682412a4f98a9f32d8b91da925
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 23:44:41 +0400

    Update 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,13 @@
+#include "main.h"
 
+/**
+ * reset_to_98 - updates the value of an integer to 98
+ * @n: pointer to the integer to be updated
+ *
+ * Description: This function takes a pointer to an int and sets
+ * the value it points to 98.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
