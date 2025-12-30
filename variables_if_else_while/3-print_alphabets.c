3a807b65d3efdc6813d12d16de5018d683dd3102
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 21:32:59 +0400

    Update 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+/**
+ * main - prints the alphabet in lowercase and uppercase followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
