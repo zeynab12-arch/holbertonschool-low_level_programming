00d20a6ddd03ef991195f6cc8cef18f580c9ecc2
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 23:30:47 +0400

    Update 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,29 @@
+#include "main.h"
 
+/**
+ * print_triangle - prints a triangle using #, followed by a new line
+ * @size: size of the triangle
+ *
+ * Description: Prints a right-aligned triangle of size `size`.
+ * If size is 0 or less, prints only a new line.
+ */
+void print_triangle(int size)
+{
+int i, j;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size - i; j++)
+_putchar(' ');
+for (j = 1; j <= i; j++)
+_putchar('#');
+
+_putchar('\n');
+}
+}
