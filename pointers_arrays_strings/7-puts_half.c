ed805e96e179747cd3a942446d8450632435d256
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 23:48:52 +0400

    Update 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * puts_half - prints the second half of a string
+ * @str: pointer to the string
+ */
+void puts_half(char *str)
+{
+int len = 0, i;
+
+while (str[len] != '\0')
+len++;
+if (len % 2 == 0)
+i = len / 2;
+else
+i = (len + 1) / 2;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+
+_putchar('\n');
+}
