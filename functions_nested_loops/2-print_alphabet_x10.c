073849dca550e72aed5e13657c448b0eaffd80e3
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2025-12-30 21:59:53 +0400

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char *alphabet = "abcdefghijklmnopqrstuvwxyz\n";
+int i;
+for (i = 0; i < 10; i++)
+{
+int j = 0;
+while (alphabet[j] != '\0')
+{
+_putchar(alphabet[j]);
+j++;
+}
+}
+}
