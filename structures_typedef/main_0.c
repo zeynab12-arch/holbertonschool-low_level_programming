5e48f59055dcb664eef8103d1057530f97df6360
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 20:01:43 +0400

    Update main_0.c

--- structures_typedef/main_0.c
+++ structures_typedef/main_0.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include "main.h"
+#include "dog.h"
 
 int main(void)
 {
-struct dog my_dog;
-init_dog(&my_dog,"Ghost",4.75,"Jon Snow");
-printf("My name is %s, and I am %.2f :) - Woof!\n",my_dog.name,my_dog.age);
-return 0;
+	struct dog my_dog;
+
+	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+	printf("My name is %s, and I am %.2f :) - Woof!\n",
+	       my_dog.name, my_dog.age);
+
+	return (0);
 }
