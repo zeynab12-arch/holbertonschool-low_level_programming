fff3e4ab507e0b7e722b75429a09b1224a28f8f3
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 18:32:55 +0400

    Update 0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,16 @@
+#include <stdlib.h>
 
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: number of bytes
+ * Return: pointer to allocated memory, exits with 98 on failure
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
+}
