db4449d9a9e3ffcb8bad9626c03935784b98720a
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 18:55:50 +0400

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,1 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
 
+/**
+ * struct dog - a structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H
