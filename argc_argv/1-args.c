473183e977b9d914ac8a56efdf1da129dd4b714e
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 18:09:15 +0400

    Update 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv; /* unused parameter */
+
+printf("%d\n", argc - 1);
+return (0);
+}
