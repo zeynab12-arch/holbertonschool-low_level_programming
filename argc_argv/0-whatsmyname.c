be1dd33b16d1a4c2fb654231bbec3906bc2fdb37
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 18:08:57 +0400

    Update 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+/**
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argc; /* unused parameter */
+
+printf("%s\n", argv[0]);
+
+return (0);
+}
