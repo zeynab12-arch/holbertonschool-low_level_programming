f7285a27ce585a2adcb0ed317e0139484feb10a6
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 21:24:44 +0400

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,4 +1,4 @@
-include <stdlib.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -11,38 +11,46 @@ include <stdlib.h>
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *d;
-        int i, name_len = 0, owner_len = 0;
+	dog_t *d;
+	int i;
+	int name_len = 0;
+	int owner_len = 0;
 
-        if (name == NULL || owner == NULL)
-                return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-        d = malloc(sizeof(dog_t));
-        if (d == NULL)
-                return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
 
-        while (name[name_len])
-                name_len++;
-        while (owner[owner_len])
-                owner_len++;
+	while (name[name_len])
+		name_len++;
 
-        d->name = malloc(sizeof(char) * (name_len + 1));
-        if (d->name == NULL)
-        {
-                free(d);
-                return (NULL);
-        }
+	while (owner[owner_len])
+		owner_len++;
 
-        d->owner = malloc(sizeof(char) * (owner_len + 1));
-        if (d->owner == NULL)
-        {
-                free(d->name);
-                free(d);
-                return (NULL);
-        }
+	d->name = malloc(sizeof(char) * (name_len + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 
-        for (i = 0; i <= name_len; i++)
-                d->name[i] = name[i];
+	d->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 
-        for (i = 0; i <= owner_len; i++)
-                                          
+	for (i = 0; i <= name_len; i++)
+		d->name[i] = name[i];
+
+	for (i = 0; i <= owner_len; i++)
+		d->owner[i] = owner[i];
+
+	d->age = age;
+
+	return (d);
+}
