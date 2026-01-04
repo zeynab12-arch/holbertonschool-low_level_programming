8d4ea161cfd673c8797cec10bf5eceef8a84aca7
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 19:28:03 +0400

    Update 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,5 @@
-##include "dog.h"
+#include "dog.h"
 
-/**
- * init_dog - initialize a struct dog
- * @d: pointer to struct dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
- */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
@@ -16,3 +9,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 }
+
