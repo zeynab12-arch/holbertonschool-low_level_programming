81ec9061aa14418876d87b0b231ce205794b1e15
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 18:09:42 +0400

    Update 2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+ * main - prints all arguments received
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+
+return (0);
+}
