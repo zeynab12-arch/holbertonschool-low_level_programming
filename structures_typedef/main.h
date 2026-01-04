583eba35ae9fa85355e62627c23df07b40fa0bfe
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 18:56:20 +0400

    Update main.h

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,1 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+/**
+ * struct dog - information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d,char *name,float age,char *owner);
+
+#endif
