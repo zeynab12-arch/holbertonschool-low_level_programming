a9a52e007c2b0de977d03cce1778b58b61833159
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-04 18:10:02 +0400

    Update 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+int a, b;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+
+printf("%d\n", a *b);
+
+return (0);
+}
