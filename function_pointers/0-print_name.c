a1deb8c56a57bebb6d39b3a12dd0f73c2e4bdd36
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-05 05:05:32 +0400

    Update 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,1 +1,16 @@
+#include "function_pointers.h"
 
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: pointer to a function that prints a name
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
