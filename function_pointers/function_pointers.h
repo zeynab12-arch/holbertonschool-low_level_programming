302098e438b66ad39c33ab8c05a8bb73eaa5239f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-05 05:30:15 +0400

    Update function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,15 @@
-##ifndef FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
+/* Task 0 */
 void print_name(char *name, void (*f)(char *));
+
+/* Task 1 */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* Task 2 */
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
