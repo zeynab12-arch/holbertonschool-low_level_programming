5a51d8087928b9b689d792657c0f092929649927
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-05 05:03:58 +0400

    Update 0-main.c

--- function_pointers/0-main.c
+++ function_pointers/0-main.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include "function_pointers.h"
 
+void print_name_as_is(char *name)
+{
+	printf("Hello, my name is %s\n", name);
+}
+
+void print_name_uppercase(char *name)
+{
+	unsigned int i = 0;
+
+	printf("Hello, my uppercase name is ");
+	while (name[i])
+	{
+		if (name[i] >= 'a' && name[i] <= 'z')
+			putchar(name[i] - 32);
+		else
+			putchar(name[i]);
+		i++;
+	}
+}
+
+int main(void)
+{
+	print_name("Bob", print_name_as_is);
+	print_name("Bob Dylan", print_name_uppercase);
+	printf("\n");
+	return (0);
+}
