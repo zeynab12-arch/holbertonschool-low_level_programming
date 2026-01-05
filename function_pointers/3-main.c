953a2c4487841d91f77c2eb6bd17be19257f8edf
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-05 05:20:26 +0400

    Update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 
+/**
+ * main - calculator program
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = f(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}
