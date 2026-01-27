5a0ccd3edd9d7c640f13f7a9f7436df29396ab8b
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 18:18:36 +0400

    Improve add_dnodeint function implementation
    Refactor add_dnodeint function for clarity and safety.

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,32 +1,25 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list
- * @head: pointer to pointer to the head of the list
- * @n: integer to add in the new node
- *
- * Return: address of the new node, or NULL if it failed
+ * add_dnodeint - adds a new node at the beginning
+ * @head: pointer to head
+ * @n: data for new node
+ * Return: address of new node, or NULL
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
-
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
 
-	if (*head != NULL)
+	if (*head)
 		(*head)->prev = new;
 
 	*head = new;
-
 	return (new);
 }
