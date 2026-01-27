e701e99f1150bcf371b47a10c94e158390146a79
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 18:32:45 +0400

    Update 8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,28 +11,28 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp;
-    unsigned int i;
+	dlistint_t *tmp;
+	unsigned int i;
 
-    if (!head || !*head)
-        return (-1);
+	if (!head || !*head)
+		return (-1);
 
-    tmp = *head;
+	tmp = *head;
 
-    for (i = 0; tmp && i < index; i++)
-        tmp = tmp->next;
+	for (i = 0; tmp && i < index; i++)
+		tmp = tmp->next;
 
-    if (!tmp)
-        return (-1);
+	if (!tmp)
+		return (-1);
 
-    if (tmp->prev)
-        tmp->prev->next = tmp->next;
-    else
-        *head = tmp->next;
+	if (tmp->prev)
+		tmp->prev->next = tmp->next;
+	else
+		*head = tmp->next;
 
-    if (tmp->next)
-        tmp->next->prev = tmp->prev;
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
 
-    free(tmp);
-    return (1);
+	free(tmp);
+	return (1);
 }
