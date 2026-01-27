c1ee0b75e2563170504d59928bc625d226ff4386
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 18:02:18 +0400

    Refactor print_dlistint function and comments
    Updated function documentation and improved readability.

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
- * @h: pointer to the first node
- * Return: number of nodes
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    return count;
+	return (count);
 }
