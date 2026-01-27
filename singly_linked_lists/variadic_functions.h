91aad8deeb3a35d056e466c4d7d3a1237e155bd1
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 17:01:37 +0400

    Add variadic function declarations in header file

--- singly_linked_lists/variadic_functions.h
+++ singly_linked_lists/variadic_functions.h
@@ -1,1 +1,18 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/* Task 0 */
+int sum_them_all(const unsigned int n, ...);
+
+/* Task 1 */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+/* Task 2 */
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/* Task 3 */
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
