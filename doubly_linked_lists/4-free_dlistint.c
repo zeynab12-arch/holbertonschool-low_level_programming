76fe083f8391c27cd35091ea2d2acf1a8c6ce0fd
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 17:37:28 +0400

    Implement function to free a doubly linked list
    This function iteratively frees each node in a doubly linked list, starting from the head.

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
 
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to the first node
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *temp;
+
+    while (head)
+    {
+        temp = head->next;
+        free(head);
+        head = temp;
+    }
+}
