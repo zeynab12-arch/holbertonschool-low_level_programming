e5e273a591363d5277d6a4b89f70710bb45e50df
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 15:10:30 +0400

    Add print_all function prototype to variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,7 +3,13 @@
 
 #include <stdarg.h>
 
+/* Task 0 / 1 */
 void print_numbers(const char *separator, const unsigned int n, ...);
+
+/* Task 2 */
 void print_strings(const char *separator, const unsigned int n, ...);
 
-#endif
+/* Task 3 */
+void print_all(const char * const format, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
