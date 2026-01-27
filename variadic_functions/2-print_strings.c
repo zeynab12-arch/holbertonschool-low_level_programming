31b6ed79eb7fd57b04c2625d4ed004b0a6d3884f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 15:04:24 +0400

    Fix separator check in print_strings function

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,4 +24,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", str);
 
-		if (separator != NUL
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(strings);
+	printf("\n");
+}
