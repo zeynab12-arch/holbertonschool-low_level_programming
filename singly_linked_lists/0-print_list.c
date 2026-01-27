d41d3b1981a42364328713c1a3c8b9f3602ac573
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 16:43:09 +0400

    Add print_list function to print list elements
    Implement print_list function to display list elements.

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
 
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the first node
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
