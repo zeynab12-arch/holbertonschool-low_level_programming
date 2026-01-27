9aef532a85120c75239b5bbe7db4dc2b8fe6189a
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 18:09:26 +0400

    Refactor sum_dlistint for clarity and consistency

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list
- * @head: pointer to the first node
- * Return: sum of all n, or 0 if the list is empty
+ * sum_dlistint - returns the sum of all the n data of a dlistint_t list
+ * @head: pointer to the head of the list
+ * Return: sum of all n, 0 if list is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
+	dlistint_t *tmp;
 
-    while (head)
-    {
-        sum += head->n;
-        head = head->next;
-    }
-
-    return sum;
+	tmp = head;
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
 }
