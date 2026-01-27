1a907a8f2a0e7829872d8feddf4fde7f718c4a38
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 14:55:53 +0400

    Refactor print_numbers function for clarity

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,22 +2,26 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list numbers;
-    unsigned int i;
+	va_list numbers;
+	unsigned int i;
 
-    va_start(numbers, n);
+	va_start(numbers, n);
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(numbers, int));
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(numbers, int));
 
-        if (separator != NULL && i < n - 1)
-            printf("%s", separator);
-    }
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
 
-    va_end(numbers);
-    printf("\n");
+	va_end(numbers);
+	printf("\n");
 }
-
