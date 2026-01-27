2bcb7d62716a93444132b01bcf849e0d7286863f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 17:56:59 +0400

    Update 1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
-int main(void)
+size_t dlistint_len(const dlistint_t *h)
 {
-    dlistint_t head;
-    size_t n;
-
-    head.n = 10;
-    head.prev = NULL;
-    head.next = NULL;
-
-    /* 1. List length test */
-    n = dlistint_len(&head);
-    printf("Length = %lu\n", n);
-
-    return (0);
+    size_t count = 0;
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
+    return (count);
 }
