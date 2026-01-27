cc6eaad3bf049b9d02c223ccc43dc13f4a9f8759
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 16:49:27 +0400

    Define singly linked list structure and functions

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,1 +1,31 @@
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* 0. Print list */
+size_t print_list(const list_t *h);
+
+/* 1. List length */
+size_t list_len(const list_t *h);
+
+/* 2. Add node at the beginning */
+list_t *add_node(list_t **head, const char *str);
+
+/* 3. Add node at the end */
+list_t *add_node_end(list_t **head, const char *str);
+
+#endif /* LISTS_H */
