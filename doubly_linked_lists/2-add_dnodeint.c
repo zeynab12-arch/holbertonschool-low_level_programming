7a9d59780631d000444f36d603a20c6be8c447f5
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-27 17:33:28 +0400

    Add function to insert node at the head of list
    Implement function to add a new node at the beginning of a doubly linked list.

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,1 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
 
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
+ * @head: pointer to the pointer to the first node
+ * @n: integer to store in the new node
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new_node;
+
+    if (!head)
+        return NULL;
+
+    new_node = malloc(sizeof(dlistint_t));
+    if (!new_node)
+        return NULL;
+
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = *head;
+
+    if (*head)
+        (*head)->prev = new_node;
+
+    *head = new_node;
+
+    return new_node;
+}
