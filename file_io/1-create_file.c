cd240fcb776d0644a83034c4784303d5a1e02eb5
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 18:27:50 +0400

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,45 +1,47 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 /**
- * create_file - creates a file and writes text to it
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write
+ * create_file - fayl yaradır və istəyə uyğun mətn yazır
+ * @filename: yaradılacaq faylın adı
+ * @text_content: fayla yazılacaq mətn (NULL ilə bitən sətir)
  *
- * Return: 1 on success, -1 on failure
+ * Return: uğurlu olarsa 1, səhv olarsa -1
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t w;
-	int len = 0;
+    int fd;
+    ssize_t bytes_written;
+    size_t len = 0;
 
-	if (filename == NULL)
-		return (-1);
+    if (filename == NULL) // Fayl adı verilməyibsə
+        return (-1);
 
-	/* calculate length of text_content */
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
-	}
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600); 
+    /* O_WRONLY - yazmaq üçün açılır
+       O_CREAT - fayl yoxdursa yaradır
+       O_TRUNC - fayl varsa mövcud mətn silinir
+       0600 - icazələr: rw------- */
+    if (fd == -1)
+        return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
+    if (text_content != NULL) // Mətn verilibsə
+    {
+        while (text_content[len])
+            len++; // Mətnin uzunluğunu hesablayırıq
 
-	if (text_content != NULL)
-	{
-		w = write(fd, text_content, len);
-		if (w == -1 || w != len)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+        bytes_written = write(fd, text_content, len);
+        if (bytes_written == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	close(fd);
-	return (1);
+    if (close(fd) == -1) // Faylı bağlaya bilmiriksə
+        return (-1);
+
+    return (1);
 }
