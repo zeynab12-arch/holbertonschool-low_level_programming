e2ad248ad09eec7ac8cc48eb3586d396d1616970
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 19:48:52 +0400

    Update 4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,1 +1,30 @@
+#include "hash_tables.h"
 
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: is the hash table you want to look into
+ * @key: is the key you are looking for
+ *
+ * Return: value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	int index;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (!strcmp(key, node->key))
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
