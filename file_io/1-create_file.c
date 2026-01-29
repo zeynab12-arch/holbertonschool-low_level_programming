f7fbbf704ce243a459bf32ed2eca8d7039ab3076
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 18:39:02 +0400

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,54 +4,41 @@
 #include <unistd.h>
 
 /**
- * create_file - fayl yaradır və istəyə uyğun mətn yazır
- * @filename: yaradılacaq faylın adı
- * @text_content: fayla yazılacaq mətn (NULL ilə bitən sətir)
+ * create_file - creates a file and writes text_content to it
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write to the file
  *
- * Return: uğurlu olarsa 1, səhv olarsa -1
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes_written;
-    size_t len = 0;
+	int fd;
+	ssize_t bytes_written;
+	size_t len;
 
-    if (filename == NULL)
-    {
-        /* Fayl adı verilməyibsə */
-        return (-1);
-    }
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    /* O_WRONLY - yazmaq üçün açılır
-       O_CREAT - fayl yoxdursa yaradır
-       O_TRUNC - fayl varsa mövcud mətn silinir
-       0600 - icazələr: rw------- */
-    if (fd == -1)
-    {
-        return (-1);
-    }
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        /* Mətn verilibsə, uzunluğunu hesablayırıq */
-        while (text_content[len])
-        {
-            len++;
-        }
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+			len++;
 
-        bytes_written = write(fd, text_content, len);
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    if (close(fd) == -1)
-    {
-        return (-1);
-    }
+	if (close(fd) == -1)
+		return (-1);
 
-    return (1);
+	return (1);
 }
