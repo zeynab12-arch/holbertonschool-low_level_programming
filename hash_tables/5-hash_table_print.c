b924c85786b2c0ff8bfbc2ace4c4f8170dc27127
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 19:49:33 +0400

    Update 5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,1 +1,33 @@
+#include "hash_tables.h"
 
+/**
+ * hash_table_print- prints a hash table.
+ * @ht: is the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int index;
+	int printed;
+
+	if (!ht)
+		return;
+
+	printed = 0;
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+		if (!node)
+			continue;
+
+		while (node)
+		{
+			if (printed++)
+				printf(", ");
+			printf("\'%s\': \'%s\'", node->key, node->value);
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
