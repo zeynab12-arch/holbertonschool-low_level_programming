cbd6a536c3eec16bb7aebf5b418424a0d02ddc17
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 18:37:04 +0400

    Update 1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,21 +16,29 @@ int create_file(const char *filename, char *text_content)
     ssize_t bytes_written;
     size_t len = 0;
 
-    if (filename == NULL) // Fayl adı verilməyibsə
+    if (filename == NULL)
+    {
+        /* Fayl adı verilməyibsə */
         return (-1);
+    }
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600); 
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
     /* O_WRONLY - yazmaq üçün açılır
        O_CREAT - fayl yoxdursa yaradır
        O_TRUNC - fayl varsa mövcud mətn silinir
        0600 - icazələr: rw------- */
     if (fd == -1)
-        return (-1);
-
-    if (text_content != NULL) // Mətn verilibsə
     {
+        return (-1);
+    }
+
+    if (text_content != NULL)
+    {
+        /* Mətn verilibsə, uzunluğunu hesablayırıq */
         while (text_content[len])
-            len++; // Mətnin uzunluğunu hesablayırıq
+        {
+            len++;
+        }
 
         bytes_written = write(fd, text_content, len);
         if (bytes_written == -1)
@@ -40,8 +48,10 @@ int create_file(const char *filename, char *text_content)
         }
     }
 
-    if (close(fd) == -1) // Faylı bağlaya bilmiriksə
+    if (close(fd) == -1)
+    {
         return (-1);
+    }
 
     return (1);
 }
