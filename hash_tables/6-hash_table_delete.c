1878f226913ba38f306939edcd441ff4b0070ba0
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 19:50:06 +0400

    Update 6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,1 +1,35 @@
+#include "hash_tables.h"
 
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: is the hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (!ht)
+		return;
+
+	for (index = 0; index < ht->size; index++)
+	{
+		node = ht->array[index];
+
+		if (!node)
+			continue;
+
+		while (node)
+		{
+			ht->array[index] = node->next;
+			free(node->key);
+			free(node->value);
+			free(node->next);
+			free(node);
+			node = ht->array[index];
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
