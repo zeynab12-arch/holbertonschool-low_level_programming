f8af8fd1476329e1a2ca3b638b5faad9c8ebd0ae
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 18:40:58 +0400

    Update 2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,41 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
  * append_text_to_file - faylın sonuna mətn əlavə edir
  * @filename: faylın adı
- * @text_content: fayla əlavə olunacaq mətn (NULL ilə bitən sətir)
+ * @text_content: NULL ilə bitən sətir
  *
- * Return: uğurlu olarsa 1, səhv olarsa -1
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes_written;
-    size_t len = 0;
+	int fd;
+	ssize_t bytes_written;
+	size_t len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
 
-        bytes_written = write(fd, text_content, len);
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	if (close(fd) == -1)
+		return (-1);
+
+	return (1);
 }
