d9bcfe419a030394118e1e2bf01fbffb71149596
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 19:48:26 +0400

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,1 +1,48 @@
+#include "hash_tables.h"
 
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table you want to add or update the key/value to.
+ * @key: is the key.
+ * @value: is the value associated with the key.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *newNode, *tmp;
+
+	if (!ht || !key)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	newNode = ht->array[index];
+	if (!newNode)
+	{
+		newNode = malloc(sizeof(hash_node_t));
+		newNode->key = strdup(key);
+		newNode->value = strdup(value);
+		newNode->next = NULL;
+		ht->array[index] = newNode;
+		return (1);
+	}
+
+	tmp = newNode;
+	while (tmp)
+	{
+		if (!strcmp(key, tmp->key))
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	newNode = malloc(sizeof(hash_node_t));
+
+	newNode->next = ht->array[index];
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+	ht->array[index] = newNode;
+	return (1);
+}
