003c9fb07aa2b4c2a06dfc1ed977e175c4afbf53
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 18:25:42 +0400

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>
+int append_text_to_file(const char *filename, char *text_content);
 
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-
-#endif /* MAIN_H */
+#endif
