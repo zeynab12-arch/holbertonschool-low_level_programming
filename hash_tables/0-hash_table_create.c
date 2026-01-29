5049a21f92014ce992231ab42f49d8156886026a
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-01-29 15:50:29 +0000

    0-6

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,1 +1,24 @@
+#include "hash_tables.h"
 
+/**
+ * hash_table_create - entry
+ * @size: size
+ * Return: hash_table_t
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	ht->size = size;
+	ht->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	return (ht);
+}
--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,1 +1,15 @@
+#include "hash_tables.h"
 
+/**
+ * key_index - gives you the index of a key.
+ * @key: key to retrieve
+ * @size: size
+ *
+ * Return: hash value
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (!key || size <= 0)
+		return (0);
+	return (hash_djb2(key) % size);
+}
