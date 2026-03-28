270c769589050dc6e260777a6fcb56e91dac5fb4
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-03-28 16:19:49 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void)
 /*
-* main - Entry point
-* Return: Always 0 (Success)
+*  main - Entry point
+*  Return: Always 0 (Success)
 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
