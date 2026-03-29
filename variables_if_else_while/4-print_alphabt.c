ae4a5a3b0952e4d636ff38022a08e9a0c3781b43
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-03-29 16:47:38 +0000

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
-* main - Entry  point
+* main - Entry point
 * Return: Always 0
 */
 
@@ -8,7 +8,7 @@ int main(void)
 {
 	char c;
 
-	for (c='a'; c <= 'z'; c++)
+	for (c ='a'; c <= 'z'; c++)
 	{
 		if (c == 'q' || c == 'e')
 		{
