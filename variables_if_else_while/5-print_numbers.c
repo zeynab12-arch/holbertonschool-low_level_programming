423590533e719f092d753caf3591aed2f1fc98b6
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-03-29 16:59:12 +0000

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+* main - Entry point
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 9; a++)
+	{
+		printf(%d, a);
+	}
+	printf("\n");
+	return (0);
+}
