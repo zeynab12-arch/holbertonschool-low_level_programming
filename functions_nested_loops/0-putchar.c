8ede5ec256562595d1cff51ba9a4c59b73c837be
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-03-30 14:29:17 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
 * main - Entry point
+*
 * Return: Always 0
 */
 
 int main(void)
 {
-	printf("_putchar\n");
+	write(1, "_putchar\n", 9);
 	return (0);
 }
