4b4a11aa047553804272aaf1bec69a972bd0e6e8
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-03-30 13:19:04 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0
+*/
+
+int main(void)
+{
+	char m;
+	char n;
+	for (m = 0; m < 10; m++)
+	{
+		putchar(m);
+	}
+	for(n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	return (0);
+}
