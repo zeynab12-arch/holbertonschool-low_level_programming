ad05ca0349d10a031c9716414945d3e46410397a
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-03-30 14:50:31 +0000

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include main.h
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	putchar('\n');
+}
