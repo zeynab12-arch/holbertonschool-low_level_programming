53c2803de5d66a6fea20e2c10c5c09d0e405fa95
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-20 14:33:48 +0000

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
-int print_sign(int n);
+int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if(n == 0)
-    {
-        _putchar('0')
-        return (0);
-    }
-    else
-    {
-        _putchar('-');
-        return(-1);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
