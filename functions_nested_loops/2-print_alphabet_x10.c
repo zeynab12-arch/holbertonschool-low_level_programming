82bed5e582e9b92f18f7b6501d21d88e71b52292
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-20 13:04:02 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,16 @@
  * print_alphabet_x10 - prints alphabet 10 times
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
-    char c;
     int i;
+    char c;
 
-    for (i = 1; i <= 10; i ++)
+    for (i = 0; i < 10; i++)
     {
         for (c = 'a'; c <= 'z'; c++)
         {
-             _putchar(c);
+            _putchar(c);
         }
         _putchar('\n');
     }
