5a8def57d8b027146714e7dc760478fcfac0f51f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-20 14:32:09 +0000

    5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,20 +2,19 @@
 
 int print_sign(int n);
 {
-    int i;
     if (n > 0)
     {
-        _putchar('+')
+        _putchar('+');
         return (1);
     }
     else if(n == 0)
     {
-        _putchar('-')
-        return (-1);
+        _putchar('0')
+        return (0);
     }
     else
     {
-        _putchar('0')
-        return(0);
+        _putchar('-');
+        return(-1);
     }
 }
