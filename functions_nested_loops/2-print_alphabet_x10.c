cad9f2ca6af2689ace0fef48c29c8edb3b5b001e
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-20 12:59:17 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 
 /**
 * main -Entry point
@@ -9,11 +9,13 @@ void print_alphabet_x10(void);
 {
     char c;
     int i;
-    for(i = 1; i <= 10; i ++)
 
+    for(i = 1; i <= 10; i ++)
+    {
         for(c = 'a'; c <= 'z'; c++)
         {
              _putchar(c);
         }
         _putchar('\n');
+    }
 }
