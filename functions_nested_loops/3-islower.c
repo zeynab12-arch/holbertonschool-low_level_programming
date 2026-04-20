e1a50dbe45f48210319b17bc7031d0320efe860c
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-20 13:26:36 +0000

    3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,8 @@
 #include "main.h"
-int _islower(int c);
+
+int _islower(int c)
 {
-    char c;
-    if(c <= 'z' && c >= 'a')
-    {
-        Return(1);
-    }
-    else
-    {
-        Return(0);
-    }
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    return (0);
 }
