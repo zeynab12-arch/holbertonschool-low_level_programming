397d065062ad15e99eccf71a80f00cb4ca0d4596
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-04-21 10:28:24 +0000

    10-11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,31 +2,31 @@
 #include "main.h"
 
 /**
-* print_to_98 - prints all natural numbers from n to 98
-* @n: strating number
-*/
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ */
 
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        while (n <= 98)
-        {
-            printf("%d",n);
-            if (n != 98)
-                printf(", ");
-            n++;
-        }
-    }
-    else
-    {
-        while (n >= 98)
-        {
-            printf("%d",n);
-            if (n != 98)
-                printf(", ");
-            n--;
-        }
-    }
-    printf("\n");
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
+		}
+	}
+	printf("\n");
 }
