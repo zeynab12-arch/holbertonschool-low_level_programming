c5f96868c72ea5cefe0ef8ea8404dcf199ea2887
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-01 09:12:30 +0000

    )

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+    int i = 0;
+
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+
+    return (i);
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
