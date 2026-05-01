0e97170af6d32a281e0a783fe66debc734e565ac
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-01 12:39:14 +0000

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,30 @@
-nano "main.h"
-char *_strcat(char *dest, char *src);
+#include "main.h"
+
 /**
-* _strcat - concanetates two strings
-* Return: dest
-*/
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
 {
     int i = 0;
     int j = 0;
 
-    while (dest[i] != 0)
-        {
-            i++
-        }
-    while (src[j] != 0)
-        {
-            dest[i] = src[j];
-            i++;
-            j++;
-        }
+    while (dest[i] != '\0')
+    {
+        i++;
+    }
 
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+
+    return (dest);
+}
