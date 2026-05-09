edb3cbd7b43b3b200a6d627d13fb87d24b89a2fc
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-09 13:47:17 +0000

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,25 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j = 0;
+	int i;
+	int j;
 
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
+	i = 0;
+	j = 0;
 
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-    dest[i] = '\0';
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    return (dest);
+	dest[i] = '\0';
+
+	return (dest);
 }
---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+*
+*
+*/
+char *cap_string(char *str)
+int i;
+int j;
