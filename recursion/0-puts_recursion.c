89624e82d8c38f717c8e042fce1a8a9c96c2beb0
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-10 19:34:57 +0000

    0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string followed by a new line
- * @s: the string to print
+ * @s: string to print
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')  /* Base case: end of string */
-{
-_putchar('\n');
-return;
-}
-
-_putchar(*s);           /* Print current character */
-_puts_recursion(s + 1); /* Recurse for the next character */
+    if (*s == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+    _putchar(*s);
+    _puts_recursion(s+1);
 }
