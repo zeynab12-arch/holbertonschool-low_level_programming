7e71eab9cfcca60d93b0204e7b3d5ff471cbbb3b
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-05-19 12:00:01 +0000

    )

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of a pointer to 98
+ * @n: pointer to an integer
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
