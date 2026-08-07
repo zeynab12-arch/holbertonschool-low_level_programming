80099009084812175ed43c5f95ae323f55abc88f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-07 14:53:57 +0000

    )

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,53 +1,50 @@
 #include "main.h"
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to POSIX stdout
- * @filename: name of the file
- * @letters: number of letters to read and print
- *
- * Return: actual number of letters read and printed, or 0 on failure
+ * read_textfile - entry
+ * @filename: filename
+ * @letters: num of letters
+ * Return: num of letters / 0
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t r, w;
-	char *buffer;
+	char *buff;
+	ssize_t bytesRead, bytesWrit;
 
 	if (filename == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
 
-	r = read(fd, buffer, letters);
-	if (r == -1)
+	buff = (char *)malloc(letters);
+	if (buff == NULL)
 	{
-		free(buffer);
+		free(buff);
 		close(fd);
 		return (0);
 	}
 
-	w = write(STDOUT_FILENO, buffer, r);
-	if (w == -1 || w != r)
+	bytesRead = read(fd, buff, letters);
+	if (bytesRead == -1)
 	{
-		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	free(buffer);
+	bytesWrit = write(STDOUT_FILENO, buff, bytesRead);
+	if (bytesWrit == -1)
+	{
+		close(fd);
+		return (0);
+	}
+
 	close(fd);
-
-	return (w);
+	return (bytesRead);
 }
