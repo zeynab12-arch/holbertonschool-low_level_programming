1aed3ed07c058e183aa14b93858bfa837971e29f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-09 18:41:05 +0000

    _

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - entry
- * @h: list
- * Return: size
+ * print_list - prints all elements of a list
+ * @h: pointer to the list
+ * Return: number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
 	const list_t *lst = h;
@@ -15,9 +15,12 @@ size_t print_list(const list_t *h)
 	while (lst != NULL)
 	{
 		s = lst->str;
+
 		if (s == NULL)
 			s = "(nil)";
+
 		printf("[%d] %s\n", lst->len, s);
+
 		i++;
 		lst = lst->next;
 	}
