6462dcdd0e379052bb79f8d154e146725dc8d481
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-09 18:07:55 +0000

    _

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string followed by a new line
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to print
  * Return: void
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
+
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
-	_puts_recursion(s + 1);
 }
