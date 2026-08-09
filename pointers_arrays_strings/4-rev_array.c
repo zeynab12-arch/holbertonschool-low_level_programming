341d789731187d33144750abc228ffed219b5f4f
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-09 18:16:42 +0000

    _

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,16 +1,19 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * reverse_array - reverses an array of integers
- * @a: array
- * @n: number of elements
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements in the array
  */
 void reverse_array(int *a, int n)
 {
-    int i = 0;
-    while (i < n)
-    {
-        a[i] = a[n - 1 - i];
-        i++;
-    }
+	int i;
+	int tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
 }
