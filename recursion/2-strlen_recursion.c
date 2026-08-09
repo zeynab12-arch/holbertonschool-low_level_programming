e7f6aa5fe2360b4d10b24769f040a48279f63b22
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-08-09 17:39:56 +0000

    _

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,10 +5,9 @@
  * @s: char
  * Return: int
  */
-
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (_strlen_recursion((s + 1)) + 1);
+	return (_strlen_recursion(s + 1) + 1);
 }
